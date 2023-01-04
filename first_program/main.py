1b5e63a4c88ecfcc3b936be4458a9d73eb257ab9
iamfobos <i_am_fobos@mail.ru>
2023-01-04 18:01:10 +0300

    version 0.2

--- first_program/main.py
+++ first_program/main.py
@@ -3,7 +3,10 @@ help - напечатать справку по программе.
 add - добавить задачу в список (название задачи запрашиваем у пользователя)
 show - напечатать все добавленые задачи.'''
 
-tasks = []
+tasks = {
+
+
+}
 
 run = True
 
@@ -14,10 +17,15 @@ while run:
     elif command == 'show':
         print(tasks)
     elif command == 'add':
-        task = input('Введите название задачи :')
-        tasks.append(task)
-        print('Задача добавлена!')
+        date = input('Введите дату задачи: ')
+        task = input('Введите название задачи: ')
+        if date in tasks:
+            tasks[date].append(task)
+        else:
+            tasks[date] = []
+            tasks[date].append(task)
+        print('Задача', task,'добавлена на дату', date,':')
     else:
         print('Неизвестная команда!')
-        run = False
+        break
 print('До свидания!')
--- first_program/test.py
+++ first_program/test.py
@@ -1,4 +1,27 @@
-x = 1
-while x <= 10:
-    print(x)
-    x += 1
+HELP = ''' 
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя)
+show - напечатать все добавленые задачи.
+exit - завершить использование программой'''
+
+tasks = []
+
+run = True
+
+while run:
+    command = input('Введите команду: ')
+    if command == 'help':
+        print(HELP)
+    elif command == 'show':
+        print(tasks)
+    elif command == 'add':
+        task = input('Введите название задачи :')
+        tasks.append(task)
+        print('Задача добавлена!')
+    elif command == 'exit':
+        print('Вы вышли из программы!')
+        break
+    else:
+        print('Неизвестная команда!')
+        break
+print('До свидания!')
