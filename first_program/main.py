5c7f1d70d65ec5c03e26501341a9c04fc6c877b0
iamfobos <i_am_fobos@mail.ru>
2023-01-04 18:16:00 +0300

    version 0.2

--- first_program/main.py
+++ first_program/main.py
@@ -1,7 +1,8 @@
 HELP = ''' 
 help - напечатать справку по программе.
-add - добавить задачу в список (название задачи запрашиваем у пользователя)
-show - напечатать все добавленые задачи.'''
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленые задачи.
+exit - завершить использование программой. '''
 
 tasks = {
 
@@ -15,7 +16,12 @@ while run:
     if command == 'help':
         print(HELP)
     elif command == 'show':
-        print(tasks)
+        date = input('Введите дату для отображения списка задач: ')
+        if date in tasks:
+            for task in tasks[date]:
+                print('- ', task)
+        else:
+            print('Такой даты нет! ')
     elif command == 'add':
         date = input('Введите дату задачи: ')
         task = input('Введите название задачи: ')
@@ -25,6 +31,11 @@ while run:
             tasks[date] = []
             tasks[date].append(task)
         print('Задача', task,'добавлена на дату', date,':')
+
+    elif command == 'exit':
+        print('Вы вышли из программы!')
+        break
+
     else:
         print('Неизвестная команда!')
         break
